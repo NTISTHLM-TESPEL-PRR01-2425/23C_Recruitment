c0e6ba3883d7e29c2fd61fca33f4314a8de7111e
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2024-10-18 14:21:26 +0200

    Initial commit

---
+++ Recruitment/Program.cs
@@ -0,0 +1,36 @@
+﻿string enemyName = "";
+
+List<string> names = ["Micke", "Lena", "Joel", "Alexander"];
+
+names.Add("Martin");
+names.Remove("Joel");
+names.RemoveAt(0);
+
+if (names.Contains("Alexander") == true)
+{
+  Console.WriteLine("yay, alexander!");
+}
+
+
+int i = Random.Shared.Next(names.Count);
+enemyName = names[i];
+
+Console.WriteLine(enemyName);
+
+/*
+List<>
+[]
+Count
+Add, Remoeve, RemoveAt
+*/
+
+
+// names[1] = "Nicholas";
+
+// for (int i = 0; i < names.Count; i++)
+// {
+//   Console.WriteLine(names[i]);
+// }
+
+
+Console.ReadLine();
